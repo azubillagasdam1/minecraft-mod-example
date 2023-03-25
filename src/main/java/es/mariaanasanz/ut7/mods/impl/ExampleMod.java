d7098708c903841d55eecea96f52c107ce87a3cc
azubillagasdam1 <azubillagasdam1@gmail.com>
2023-03-25 12:00:38 +0100

    Al fin corre el juego

--- src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
+++ src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
@@ -3,19 +3,30 @@ package es.mariaanasanz.ut7.mods.impl;
 import es.mariaanasanz.ut7.mods.base.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Minecart;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
+import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -33,7 +44,11 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
         IInteractEvent, IMovementEvent {
 
 
-    private boolean yaLlamado = false;//usada para controlar los bucles infinitos que se crean al llamar a "MovementInputUpdateEvent movement"
+ //   private boolean yaLlamado = false;//usada para controlar los bucles infinitos que se crean al llamar a "MovementInputUpdateEvent movement"
+     private BlockPos posicionJugador;
+     private BlockPos posicionBloqueDebajo;
+    double PosXAnterior = Integer.MAX_VALUE;
+    double PosZAnterior = Integer.MAX_VALUE;
 
     public ExampleMod(){
         super();
@@ -64,12 +79,12 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
     }*/
 
     /*Devuelve las cordenadas en directo de el bloque */
-    @SubscribeEvent
+   /* @SubscribeEvent
     public void printCordenadasJugador(MovementInputUpdateEvent movement) {
         System.out.print("X: "+(int)movement.getEntity().xOld+"\t");
         System.out.print("Y: "+(int)movement.getEntity().yOld+"\t");
-        System.out.print("Z: "+(int)movement.getEntity().zOld+"\t");
-        System.out.println("");
+        System.out.print("Z: "+(int)movement.getEntity().yOld+"\t");
+
     }
 
     @SubscribeEvent
@@ -77,59 +92,158 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
         System.out.print("X: "+(int)movement.getEntity().xOld+"\t");
         System.out.print("Y: "+(int)(movement.getEntity().yOld-1)+"\t");
         System.out.print("Z: "+(int)movement.getEntity().zOld+"\t");
-        System.out.println("");
+
+    }*/
+
+
+
+    /*Devuelve el BlockPos de del jugador*/
+   @SubscribeEvent
+    public void PosicionJugador( MovementInputUpdateEvent movement) {
+
+        BlockPos posicion = new BlockPos(movement.getEntity().xOld, movement.getEntity().yOld, movement.getEntity().zOld);
+        //System.out.println(posicion.toString());
+        this.posicionJugador = posicion;
     }
-
-
-    /*Devuelve el BlockPos de el bloque de abajo del jugador*/
     @SubscribeEvent
-    public BlockPos getBloqueDeAbajo(MovementInputUpdateEvent movement) {
-        BlockPos posicion = new BlockPos(movement.getEntity().xOld,(movement.getEntity().yOld-1),movement.getEntity().zOld);
-        return posicion;
+    public void PosicionBloqueDebajo( MovementInputUpdateEvent movement) {
+
+        BlockPos posicion = new BlockPos(movement.getEntity().xOld, movement.getEntity().yOld-1, movement.getEntity().zOld);
+        //System.out.println(posicion.toString());
+
+        this.posicionBloqueDebajo = posicion;
     }
 
+
+
+
+
+/*
+ @SubscribeEvent
+    public BlockPos getPosicionJugador() {
+            Player player = Minecraft.getInstance().player;
+        BlockPos playerPos = new BlockPos(player.getOnPos().getX(), player.getOnPos().getY(), player.getOnPos().getX());
+            System.out.println("La posición actual del jugador es: " + playerPos);
+    return  playerPos;}
+ */
     /**/
-    @SubscribeEvent
+    /*@SubscribeEvent
     public void reemplazarBloque(MovementInputUpdateEvent movement) {
-        yaLlamado = false;
+       // yaLlamado = false;
         selectorDeBotas(movement);
-        System.out.println("Se ejecutó el método reemplazarBloque");
-    }
+       // System.out.println("Se ejecutó el método reemplazarBloque");
+    }*/
 
 
     /*Comprueba si el jugador lleva botas, y que tipo de botas para llamar a sus respectivos metodos*/
-    @SubscribeEvent
+   @SubscribeEvent
     public void selectorDeBotas(MovementInputUpdateEvent movement) {
-        System.out.println("Se ejecuto:botasQueColocanBlock0");
-        int blockId = 1;
-        if (!yaLlamado) {
-            colocarBloque(blockId, movement);
-            yaLlamado = true;
-        }
+        ItemStack botas = Minecraft.getInstance().player.getItemBySlot(EquipmentSlot.FEET);
+
+            if (botas.getItem().equals( Items.DIAMOND_BOOTS)) {
+                System.out.println("DIAMOND_BOOTS");
+            }
+            if (botas.getItem().equals( Items.GOLDEN_BOOTS)) {
+                System.out.println("GOLDEN_BOOTS");
+            }
+            if (botas.getItem().equals( Items.IRON_BOOTS)) {
+                System.out.println("IRON_BOOTS");
+            }
+            if (botas.getItem().equals( Items.LEATHER_BOOTS)) {
+                //System.out.println("LEATHER_BOOTS");
+                botasCuero();
+                colocarBloque(1);
+
+            }else{
+                System.out.println("NO LLEVAS NADA");
+            }
+
+
+
     }
-    /*@SubscribeEvent
-    public void pruebas() {
-        System.out.println("Se ejecuto:botasQueColocanBlock0");
-        Player.getEquipmentSlotForItem(ItemStack.of(ItemStack.))
-    }*/
+
+
+
+    public void botasCuero() {
+        if(cambioDeBloque()){
+            if (Math.random() < 0.5) {
+                colocarBloque(1);
+                System.out.println("ID:1");
+            }else{colocarBloque(52);
+                System.out.println("ID:2");
+
+            }
+            }else{
+            System.out.println("No se esta ejecutando BotasCuero()");
+        }
+
+
+
+
+
+        }
+
+        /*Este metodo si lo llamas, comprueba desde la ultima vez que lo has llamado y devueve true si has cambiado de bloque*/
+
+
+        public boolean cambioDeBloque(){
+            if(((int)posicionJugador.getX())!=((int)PosXAnterior)&&PosXAnterior!=Integer.MAX_VALUE){
+                System.out.println("Has cambiado de bloque X");
+                this.PosXAnterior = posicionJugador.getX();
+                this.PosZAnterior = posicionJugador.getZ();
+
+                return true;
+            } else if (((int)posicionJugador.getZ())!=((int)PosZAnterior)&&PosZAnterior!=Integer.MAX_VALUE){
+                System.out.println("Has cambiado de bloque Z");
+                this.PosXAnterior = posicionJugador.getX();
+                this.PosZAnterior = posicionJugador.getZ();
+
+                return true;
+                }else{
+                this.PosXAnterior = posicionJugador.getX();
+                this.PosZAnterior = posicionJugador.getZ();
+
+            }
+
+            return false;
+        }
+
+
+
+
 
     /*Coloca un bloque que recibe como parametro*/
-    public void colocarBloque(int blockId, MovementInputUpdateEvent movement) {
-        System.out.println("Se ejecuto:colocarBloque");
-        BlockPos coordenadas = getBloqueDeAbajo(movement);
-        movement.getEntity().getLevel().setBlock(coordenadas, Block.stateById(blockId), 512);
+  /* public void colocarBloque(int blockId, MovementInputUpdateEvent movement) {
+       // System.out.println("Se ejecuto:colocarBloque");
+        BlockPos coordenadas = getPosicionJugador();
+        //movement.getEntity().getLevel().setBlock(coordenadas, Block.stateById(blockId), 512);
         Minecraft.getInstance().level.setBlock(coordenadas, Block.stateById(blockId), 512);
+    }*/
+
+
+    public void colocarBloque(int blockId) {
+        // System.out.println("Se ejecuto:colocarBloque");
+        BlockPos coordenadas = posicionBloqueDebajo;
+        Minecraft.getInstance().level.setBlock(coordenadas, Block.stateById(blockId), 512);
+       // movement.getEntity().getLevel().setBlock(coordenadas, Block.stateById(30), 512);
+        //movement.getEntity().getLevel().setBlock(coordenadas, Block.stateById(blockId), 512);
     }
 
-      /* @SubscribeEvent
-    public void botasQueColocanBlock0(MovementInputUpdateEvent movement) {
-        if (!yaLlamado) {
-            yaLlamado = true;
-            BlockPos coordenadas = getBloqueDeAbajo(movement);
-            movement.getEntity().getLevel().setBlock(coordenadas, Block.stateById(1), 512);
-            Minecraft.getInstance().level.setBlock(coordenadas, Block.stateById(2), 512);
-        }
-    }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Override
@@ -193,8 +307,7 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
             }
         }
     }
-
-    @Override
+       @Override
     @SubscribeEvent
     public void onPlayerWalk(MovementInputUpdateEvent event) {
         if(event.getEntity() instanceof Player){
